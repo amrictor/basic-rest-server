46769fd1230b884c57dd180ec8b2bdff368d0936
Abigail Rictor <amrictor@gmail.com>
2020-09-15 10:09:01 -0600

    Update auth.js
    uncomment authentication code lol

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,7 +3,7 @@ const security = require('../utils/security')
 module.exports = (req, res, next) => {
   try {
     const token = req.headers['x-access-token'];
-    req.userId = 'ID' //security.authenticate(token)
+    req.userId = security.authenticate(token)
     next();
   } catch {
    res.status(401).json({ auth: false, message: 'Failed to authenticate token.' })
